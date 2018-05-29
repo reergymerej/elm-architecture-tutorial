85be1e878f94aa55c173878d212228ff2aecee8d
Jeremy Greer <jeremy.greer@engageonq.com>
2018-05-29 09:47:05 -0400

    Add filterable task list

--- examples/00-kitchen-sink.elm
+++ examples/00-kitchen-sink.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html
+import Html.Events
 
 
 main =
@@ -11,13 +12,114 @@ main =
         }
 
 
+type alias Task =
+    { task : String
+    , complete : Bool
+    }
+
+
+type Visibility
+    = All
+    | Active
+    | Completed
+
+
+type alias TaskList =
+    List Task
+
+
+type alias Model =
+    { todoItems : TaskList
+    , visiblity : Visibility
+    }
+
+
+model : Model
 model =
-    0
+    { todoItems =
+        [ { task = "Buy milk"
+          , complete = True
+          }
+        , { task = "Drink milk"
+          , complete = False
+          }
+        ]
+    , visiblity = All
+    }
 
 
-update model =
-    model
+type Msg
+    = SetVisibilityActive
+    | SetVisibilityAll
+    | SetVisibilityComplete
 
 
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetVisibilityAll ->
+            { model | visiblity = All }
+
+        SetVisibilityActive ->
+            { model | visiblity = Active }
+
+        SetVisibilityComplete ->
+            { model | visiblity = Completed }
+
+
+renderTodoItems : TaskList -> Html.Html Msg
+renderTodoItems list =
+    Html.ul []
+        (List.map (\item -> Html.li [] [ Html.text item.task ]) list)
+
+
+renderTodoVisibilityButton : String -> Msg -> Html.Html Msg
+renderTodoVisibilityButton text message =
+    Html.button
+        [ Html.Events.onClick message
+        ]
+        [ Html.text text ]
+
+
+renderTodoButtons : Html.Html Msg
+renderTodoButtons =
+    Html.div []
+        [ renderTodoVisibilityButton "Show All" SetVisibilityAll
+        , renderTodoVisibilityButton "Show Active" SetVisibilityActive
+        , renderTodoVisibilityButton "Show Completed" SetVisibilityComplete
+        ]
+
+
+filterTodoItems : TaskList -> Visibility -> TaskList
+filterTodoItems taskList visibility =
+    case visibility of
+        All ->
+            taskList
+
+        Active ->
+            List.filter (\task -> task.complete == False) taskList
+
+        Completed ->
+            List.filter (\task -> task.complete == True) taskList
+
+
+renderTodoStuff : Model -> Html.Html Msg
+renderTodoStuff model =
+    Html.div []
+        [ renderTodoItems (filterTodoItems model.todoItems model.visiblity)
+        , renderTodoButtons
+        ]
+
+
+view : Model -> Html.Html Msg
 view model =
-    Html.div [] [ Html.text "Hi" ]
+    Html.div []
+        [ Html.h1 [] [ Html.text "Kitchen Sink" ]
+        , Html.div []
+            [ Html.h2 [] [ Html.text "Union Types" ]
+            , Html.div []
+                [ Html.h3 [] [ Html.text "todo list" ]
+                , renderTodoStuff model
+                ]
+            ]
+        ]
